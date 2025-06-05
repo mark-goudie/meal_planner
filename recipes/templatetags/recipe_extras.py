555509d930dcf6b6af58741452a8f287addaf96f
Mark Goudie <goudie.mark@gmail.com>
2025-06-05 22:16:16 +1000

    updates to main.css

---
+++ recipes/templatetags/recipe_extras.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def get_meal(plans, meal_type):
+    """
+    Returns the first plan in the list with the given meal_type, or None.
+    """
+    for plan in plans:
+        if getattr(plan, 'meal_type', None) == meal_type:
+            return plan
+    return None
