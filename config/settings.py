1620cfea89a6c8bc9744514ab7e7f157ea8eaf0b
Mark Goudie <goudie.mark@gmail.com>
2025-06-08 22:25:58 +1000

    media queries

--- config/settings.py
+++ config/settings.py
@@ -136,3 +136,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
